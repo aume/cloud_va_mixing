c65b646672915fed38acb7b7ddf4d342bb799fbf
Miles <mthorogo@sfu.ca>
2017-06-08 08:50:15 -0700

    Added controlmix.js for implementing audio mixing with VA envelopes

---
+++ js/controlmix.js
@@ -0,0 +1,21 @@
+/*
+**
+**
+** controlmix.js
+** implements PD controller for va mixing a roughmix
+**
+**
+*/
+
+
+// @param - tracks, semgment pool, va envelopes
+function ControlMix() {
+
+}
+
+
+ControlMix.prototype.mixit = function(first_argument) {
+	// body...
+};
+
+
